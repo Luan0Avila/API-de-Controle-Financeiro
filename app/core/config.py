5b21496642ef2fbb976c7f74fdd4ab4b0eb4fd2e
Luan0AVila <avila.almeida.luan@gmail.com>
2026-02-10 12:53:44 -0300

    criando o registro de user e adicionando segurança com jwt

---
+++ app/core/config.py
@@ -0,0 +1,5 @@
+from datetime import timedelta
+
+SECRET_KEY = "AKAFAKFLKAJFLAKJ@!1293084309fkdokfj091u0jdpsjdpoaj!@)()*"
+ALGORITHM = "HS256"
+ACCES_TOKEN_EXPIE_MINUTES = 60
---
+++ app/routes/auth.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from datetime import timedelta
+
+from app.database.session import get_db
+from app.models.user import User
+from app.schemas.user import UserCreate, UserLogin
+from app.core.security import hash_password, verify_password, create_acess_token
+from app.core.config import ACCES_TOKEN_EXPIE_MINUTES
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+@router.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    exists = db.query(User).filter(User.email == User.email).first()
+    if exists:
+        raise HTTPException(status_code=400, detail="E-mail já cadastrado")
+
+    new_user = User(
+        email=user.email,
+        password=hash_password(user.password))
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "Usuário criado com sucesso"}
+
