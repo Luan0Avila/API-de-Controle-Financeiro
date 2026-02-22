346e7ab16c27871ac39752978865c9e301fd3e4f
Luan0AVila <avila.almeida.luan@gmail.com>
2026-02-22 10:19:35 -0300

    fazendo um sumario por mes

--- app/routes/transactions.py
+++ app/routes/transactions.py
@@ -5,8 +5,10 @@ from app.models.transaction import Transaction
 from app.schemas.transaction import TransactionCreate, TransactionResponse, FinancialSummary
 from app.core.security import get_current_user
 from app.models.user import User
-from typing import List
-from app.services.finance import calculate_summary
+from typing import List, Optional
+from app.services.finance import calculate_summary, calculate_monthly_summary
+from datetime import date
+from sqlalchemy import extract
 
 router = APIRouter(prefix="/transactions", tags=["Transactions"])
 
@@ -28,12 +30,22 @@ def create_transaction(
 
 @router.get("/", response_model=List[TransactionResponse])
 def list_transactions(
+    month: Optional[int] = None,
+    year: Optional[int] = None,
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)):
 
-    return db.query(Transaction).filter(
+    query = db.query(Transaction).filter(
         Transaction.user_id == current_user.id
-    ).all()
+    )
+
+    if month & year:
+        query = query.filter(
+            extract("month", Transaction.date) == month,
+            extract("year", Transaction.date) == year
+        )
+
+    return query.all()
 
 @router.get(
     "/summary", response_model=FinancialSummary)
@@ -42,3 +54,21 @@ def get_summary(
     current_user: User = Depends(get_current_user)
 ):
     return calculate_summary(db, current_user.id)
+
+
+@router.get(
+    "/summary/month",
+    response_model=FinancialSummary
+)
+def get_monthly_summary(
+    month: int,
+    year: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    return calculate_monthly_summary(
+        db,
+        current_user.id,
+        month,
+        year
+    )
--- app/services/finance.py
+++ app/services/finance.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import func
+from sqlalchemy import func, extract
 from app.models.transaction import Transaction
 
 def calculate_summary(db: Session, user_id: int):
@@ -13,6 +13,30 @@ def calculate_summary(db: Session, user_id: int):
         Transaction.type == "expense"
     ).scalar() or 0
     
+    balance = income - expense
+
+    return {
+        "total_income": income,
+        "total_expense": expense,
+        "balance": balance
+    }
+
+
+def calculate_monthly_summary(db: Session, user_id: int, month: int, year: int):
+    income = db.query(func.sum(Transaction.value)).filter(
+        Transaction.user_id == user_id,
+        Transaction.type == "income",
+        extract("month", Transaction.date) == month,
+        extract("year", Transaction.date) == year
+    ).scalar() or 0
+
+    expense = db.query(func.sum(Transaction.value)).filter(
+        Transaction.user_id == user_id,
+        Transaction.type == "expense",
+        extract("month", Transaction.date) == month,
+        extract("year", Transaction.date) == year
+    ).scalar() or 0
+
     balance = income - expense
 
     return {
